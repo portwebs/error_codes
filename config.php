43281063d30da4f44353cf4ecb466682fd2c5ac7
Daniel <dannyseiner@gmail.com>
2021-01-25 19:19:07 +0100

    basic code list

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+	$error_codes_list = array(
+		"400"=>array(
+			"header"=>"Database connection error!",
+			"text"=>"There has been error while connecting to the database, please contact the owner of the website!"
+		),
+		"404"=>array(
+			"header"=>"Page not found!",
+			"text"=>"This page doesnt exists, please redirect back to home page!"
+		)
+	);
+
+?>
