cb96c0580791da48d846173df81989f6350e8454
Daniel <dannyseiner@gmail.com>
2021-01-25 19:56:19 +0100

    more errors

--- config.php
+++ config.php
@@ -4,10 +4,22 @@
 			"header"=>"Database connection error!",
 			"text"=>"There has been error while connecting to the database, please contact the owner of the website!"
 		),
+		"400-1"=>array(
+			"header"=>"Delete failed! 400-1",
+			"text"=>"This page doesnt exists, please redirect back to home page!"
+		),
+		"400-2"=>array(
+			"header"=>"Insert failed! 400-2",
+			"text"=>"This page doesnt exists, please redirect back to home page!"
+		),
+		"400-3"=>array(
+			"header"=>"Update failed! 400-3",
+			"text"=>"This page doesnt exists, please redirect back to home page!"
+		),
 		"404"=>array(
 			"header"=>"Page not found!",
 			"text"=>"This page doesnt exists, please redirect back to home page!"
-		)
+		),
 	);
 
 ?>
