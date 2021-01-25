4f9fbc02fe254891c7fd44e15507d6b5958f8db2
Daniel <dannyseiner@gmail.com>
2021-01-25 20:03:54 +0100

    .env codes

--- config.php
+++ config.php
@@ -1,5 +1,17 @@
 <?php 
 	$error_codes_list = array(
+		"401-1"=>array(
+			"header"=>".env database",
+			"text"=>"There has been error while connecting to the database, please contact the owner of the website!"
+		),
+		"401-2"=>array(
+			"header"=>".env htaccess",
+			"text"=>"There has been error while connecting to the database, please contact the owner of the website!"
+		),
+		"401-3"=>array(
+			"header"=>".env mode",
+			"text"=>"There has been error while connecting to the database, please contact the owner of the website!"
+		),
 		"400"=>array(
 			"header"=>"Database connection error!",
 			"text"=>"There has been error while connecting to the database, please contact the owner of the website!"
